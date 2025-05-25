b3d03d70e750b8783ae4ec727a5d7fa4f62c4e42
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2025-05-25 14:22:42 +0700

    Domain Layer - AuthRepository

---
+++ lib/features/auth/domain/auth_repository.dart
@@ -0,0 +1,14 @@
+import 'package:fpdart/fpdart.dart';
+import '../../../core/error/failure.dart';
+
+abstract interface class AuthRepository {
+  Future<Either<Failure, String>> signUpWithEmailPassword({
+    required String name,
+    required String email,
+    required String password,
+  });
+  Future<Either<Failure, String>> loginWithEmailPassword({
+    required String email,
+    required String password,
+  });
+}
