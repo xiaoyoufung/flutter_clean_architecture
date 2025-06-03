0bd5b87f9dc7f3898accdab865ce5f2b8b9f8b6f
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2025-06-03 17:05:40 +0700

    Blog Card UI

--- lib/features/blog/presentation/pages/blog_page.dart
+++ lib/features/blog/presentation/pages/blog_page.dart
@@ -1,13 +1,16 @@
 import 'package:blog_app/core/common/widgets/loader.dart';
+import 'package:blog_app/core/theme/app_palette.dart';
 import 'package:blog_app/core/utils/show_snackbar.dart';
 import 'package:blog_app/features/blog/presentation/bloc/blog_bloc.dart';
 import 'package:blog_app/features/blog/presentation/pages/add_new_blog_page.dart';
+import 'package:blog_app/features/blog/presentation/widgets/blog_card.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class BlogPage extends StatefulWidget {
   static route() => MaterialPageRoute(builder: (context) => BlogPage());
+
   const BlogPage({super.key});
 
   @override
@@ -15,13 +18,12 @@ class BlogPage extends StatefulWidget {
 }
 
 class _BlogPageState extends State<BlogPage> {
-  
   @override
   void initState() {
     super.initState();
     context.read<BlogBloc>().add(BlogFetchAllBlogs());
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,27 +38,31 @@ class _BlogPageState extends State<BlogPage> {
           ),
         ],
       ),
-      body: BlocConsumer<BlogBloc, BlogState>(
-        listener: (context, state){
-          if(state is BlogFailure){
-            showSnackbar(context, state.error);
-          }
-        },
-        builder: (context, state) {
-          if(state is BlogLoading){
-            return const Loader();
-          }
-          if(state is BlogDisplaySuccess){
-            return ListView.builder(
-                itemCount: state.blogs.length,
-                itemBuilder: (context, index){
-                  final blog = state.blogs[index];
-                  return Text(blog.title);
-                });
-          }
-          return const SizedBox();
+      body: BlocConsumer<BlogBloc, BlogState>(listener: (context, state) {
+        if (state is BlogFailure) {
+          showSnackbar(context, state.error);
         }
-      ),
+      }, builder: (context, state) {
+        if (state is BlogLoading) {
+          return const Loader();
+        }
+        if (state is BlogDisplaySuccess) {
+          return ListView.builder(
+              itemCount: state.blogs.length,
+              itemBuilder: (context, index) {
+                final blog = state.blogs[index];
+                return BlogCard(
+                  blog: blog,
+                  color: index % 3 == 0
+                      ? AppPallete.gradient1
+                      : index % 3 == 1
+                          ? AppPallete.gradient2
+                          : AppPallete.gradient3,
+                );
+              });
+        }
+        return const SizedBox();
+      }),
     );
   }
 }
